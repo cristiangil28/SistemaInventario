4e3f6a051243b29162e561ac40520dada3e662d6
cristian gil <45280035+cristiangil28@users.noreply.github.com>
2019-04-29 11:13:36 -0500

    Add files via upload

--- resources/views/Factura/create.blade.php
+++ resources/views/Factura/create.blade.php
@@ -3,6 +3,16 @@
 
 
 			<div class="container">
+					@if (count($errors) > 0)
+					<div class="alert alert-danger">
+						<strong>Error!</strong> Revise los campos obligatorios.<br><br>
+						<ul>
+							@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+					@endif
 				<div class="panel-heading">
 					<h3 class="panel-title " style="text-align:center">Crear Factura</h3>
 				</div>
